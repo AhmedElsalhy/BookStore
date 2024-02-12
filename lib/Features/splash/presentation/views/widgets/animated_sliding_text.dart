afee608c3a3dd78ef8f120e787c6e8585ad03b3e
AhmedElsalhy <Ahmedelsalhy8989@gmail.com>
2024-02-12 17:26:18 +0200

    adding sliding text animation to splash

---
+++ lib/Features/splash/presentation/views/widgets/animated_sliding_text.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AnimatedSlidingText extends StatelessWidget {
+  const AnimatedSlidingText({
+    super.key,
+    required this.slidingAnimation,
+  });
+
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: slidingAnimation,
+        builder: (context, _) {
+          return SlideTransition(
+            position: slidingAnimation,
+            child: const Text(
+              'Discover the Magic of Books...',
+              textAlign: TextAlign.center,
+            ),
+          );
+        });
+  }
+}

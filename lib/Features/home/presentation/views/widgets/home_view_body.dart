99209be5ffaddbdfeff510c3c03969716fec4337
AhmedElsalhy <Ahmedelsalhy8989@gmail.com>
2024-02-14 18:39:39 +0200

    add styles and create bestSeller ListView Item

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:book_store/core/utils/assets_paths.dart';
 import 'package:book_store/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -10,7 +11,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
+      padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -21,7 +22,56 @@ class HomeViewBody extends StatelessWidget {
           ),
           Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          BestSellerListViewItem()
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.4 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.white,
+                image: const DecorationImage(
+                  image: AssetImage(
+                    AssetsPath.logo,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 30,
+          ),
+          Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: const Text(
+                  'Harry Potter and the Goblet of Fire',
+                  style: Styles.textStyle20,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+              ),
+            ],
           )
         ],
       ),
--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,8 +1,26 @@
+import 'package:book_store/constants.dart';
 import 'package:flutter/cupertino.dart';
 
 abstract class Styles {
-  static const titleMedium = TextStyle(
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+  );
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const textStyle18 = TextStyle(
     fontWeight: FontWeight.w600,
     fontSize: 18,
+  );
+  static const textStyle20 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+    fontFamily: kGtSectraFine,
+  );
+  static const textStyle30 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 30,
+    fontFamily: kGtSectraFine,
   );
 }

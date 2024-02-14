9d261ec877061dc3b4d7564ba0466614b356ed2c
AhmedElsalhy <Ahmedelsalhy8989@gmail.com>
2024-02-14 14:31:49 +0200

    add feature list view to home

--- lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -1,24 +1,21 @@
 import 'package:book_store/core/utils/assets_paths.dart';
 import 'package:flutter/material.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class CustomBookItem extends StatelessWidget {
+  const CustomBookItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        aspectRatio: 1.3 / 2,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: Colors.white,
-            image: const DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage(
-                AssetsPath.logo,
-              ),
+    return AspectRatio(
+      aspectRatio: 1.3 / 2,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.white,
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(
+              AssetsPath.logo,
             ),
           ),
         ),
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
-import 'custom_list_view_item.dart';
+import 'featured_books_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        CustomListViewItem(),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+        ],
+      ),
     );
   }
 }
